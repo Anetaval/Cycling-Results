a2b42957a6c91853f13552792e94221a871199a9
Aneta Valkova <anetka@Aneta-MacBook-Air.local>
2025-03-11 17:08:21 +0100

    Fix: Remove basename for Netlify deploy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import React from "react";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home";
-import EventDetail from "./EventDetail"; // Importujeme EventDetail
-import DisciplineLive from "./DisciplineLive"; // Přidáme import
+import EventDetail from "./EventDetail";
+import DisciplineLive from "./DisciplineLive";
 
 function App() {
   return (
-    <Router basename="/Cycling-Results">
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/event/:eventName" element={<EventDetail />} />
         <Route path="/event/:eventName/discipline/:disciplineName" element={<DisciplineLive />} />
-        {/* Další cesty přidáme později */}
       </Routes>
     </Router>
   );
 }
 
 export default App;
-
-
-
