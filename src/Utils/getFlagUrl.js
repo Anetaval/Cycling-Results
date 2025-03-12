d445f2a7544e6e74439615f592b51e496b35a54e
Anetaval <valkova.aneta@gmail.com>
2025-03-13 00:03:07 +0100

    Delete src directory

--- src/Utils/getFlagUrl.js
+++
@@ -1,7 +0,0 @@
-import countryCodeMap from '../data/countryCodeMap.json';
-
-export function getFlagUrl(alpha3) {
-  const alpha2 = countryCodeMap[alpha3];
-  if (!alpha2) return ''; // když stát nenajdeme
-  return `https://flagcdn.com/w40/${alpha2.toLowerCase()}.png`; // velikost vlajky (změň na w80, w20 podle potřeby)
-}
